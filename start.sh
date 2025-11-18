ffa77ba2cf388ba6d5c99e0938726869c8df38b4
retr0sity <ioannis.karkalas@gmail.com>
2025-11-18 14:00:30 +0100

    Removed composer completely, hoping my dependences are present.

--- start.sh
+++ start.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Install Composer if not exists
-if ! command -v composer &> /dev/null; then
-    curl -sS https://getcomposer.org/installer | php
-    mv composer.phar /usr/local/bin/composer
-    chmod +x /usr/local/bin/composer
-fi
-
-# Install dependencies
 cd restApi
-composer install --no-dev --optimize-autoloader
-
-# Start the server
 php -S 0.0.0.0:$PORT index.php
