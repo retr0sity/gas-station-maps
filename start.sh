0d89e841151e6f1dd2e4dcc7bc205337bed47461
retr0sity <ioannis.karkalas@gmail.com>
2025-11-18 13:53:50 +0100

    Until my last breath, I will never surrender to composer not installing. The leaves may wither and the lake might dry, one day composer will succumb to my will. This, I promise.

--- start.sh
+++ start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Copy the existing composer.json from restApi if needed
-cp restApi/composer.json ./
+# Install Composer if not exists
+if ! command -v composer &> /dev/null; then
+    curl -sS https://getcomposer.org/installer | php
+    mv composer.phar /usr/local/bin/composer
+    chmod +x /usr/local/bin/composer
+fi
 
 # Install dependencies
+cd restApi
 composer install --no-dev --optimize-autoloader
 
-# Start the server from restApi directory
-cd restApi
+# Start the server
 php -S 0.0.0.0:$PORT index.php
